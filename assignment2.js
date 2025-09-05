8562b05dcc0703cd84e5274e5b87853c64149453
kiranbkvb6 <kiranbkvb6@gmail.com>
2025-09-05 13:40:42 +0530

    a2 is done

---
+++ assignment2.js
@@ -0,0 +1,11 @@
+//Write a function sum that finds the sum of two numbers. 
+//Side quest - Try passing in a string instead of a number and see what happens?
+
+function sum(x,y){
+    let result=x+y;
+    console.log("The sum of "+x+" and "+y+" is: "+result);
+    return result;
+}
+
+let finalResult=sum(34,67);
+console.log('The Result of then sum of two numbers 34 & 67 is :'+finalResult);
