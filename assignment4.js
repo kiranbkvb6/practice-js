29f9e090f1b2058215d686e41a6fbdc655b71402
kiranbkvb6 <kiranbkvb6@gmail.com>
2025-09-05 14:28:06 +0530

    assignment 4

---
+++ assignment4.js
@@ -0,0 +1,12 @@
+function eveodd(num){
+    if(num%2==0){
+        console.log(num+" is even number");
+    }else{
+        console.log(num+" is odd number");
+    }
+}
+eveodd(5);
+eveodd(10);
+eveodd(0);
+eveodd(-3);
+eveodd(-8);
