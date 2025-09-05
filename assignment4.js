2c7991242ced1778e5963cde7c6292a42a331142
kiranbkvb6 <kiranbkvb6@gmail.com>
2025-09-05 14:29:11 +0530

    update as4

--- assignment4.js
+++ assignment4.js
@@ -1,3 +1,5 @@
+//Write an if/else statement that checks if a number is even or odd. If it's even, print "The number is even." Otherwise, print "The number is odd."
+
 function eveodd(num){
     if(num%2==0){
         console.log(num+" is even number");
